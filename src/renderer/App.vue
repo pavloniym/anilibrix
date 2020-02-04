81ac9f78eb6c4d0722ef32ce8190587320da556e
PavelShar <pavloniym@gmail.com>
2020-02-04 20:53:42 +0300

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'anilibria-theater',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
