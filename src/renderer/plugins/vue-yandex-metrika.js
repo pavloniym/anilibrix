a0057aa29937a43a386912e7eb0c5bdc904a9b73
Pavel Sharypov <pavloniym@gmail.com>
2020-06-11 22:05:20 +0300

    fix(App): fixed yandex metrika plugin initialization

--- src/renderer/plugins/vue-yandex-metrika.js
+++ src/renderer/plugins/vue-yandex-metrika.js
@@ -5,32 +5,32 @@ import __get from 'lodash/get'
 
 export default {
   install: (Vue, {id, store, router}) => {
-
-    Vue.use(VueYandexMetrika, {
-      id,
-      router,
-      env: 'production', //process.env.NODE_ENV,
-      options: {
-        params: {
-          userId: __get(store, 'state.app.account.userId'),
-          version: app.version,
-          autoplayNext: __get(store, 'state.app.settings.player.autoplayNext'),
-          playerQuality: __get(store, 'state.app.settings.player.quality'),
-          connectionHost: __get(store, 'state.app.settings.connection.host'),
-          connectionType: __get(store, 'state.app.settings.connection.proxy.type'),
-          autoRefreshUsage: __get(store, 'state.app.settings.system.updates.enabled'),
-          autoRefreshTimeout: __get(store, 'state.app.settings.system.updates.timeout'),
-          torrentsProcessing: __get(store, 'state.app.settings.player.torrents.process'),
-        },
-        webvisor: true,
-        clickmap: true,
-        trackHash: true,
-        userParams: {
-          UserID: __get(store, 'state.app.account.userId'),
-        },
-        accurateTrackBounce: true,
-      }
-    })
-
+    if(id) {
+      Vue.use(VueYandexMetrika, {
+        id,
+        router,
+        env: process.env.NODE_ENV,
+        options: {
+          params: {
+            userId: __get(store, 'state.app.account.userId'),
+            version: app.version,
+            autoplayNext: __get(store, 'state.app.settings.player.autoplayNext'),
+            playerQuality: __get(store, 'state.app.settings.player.quality'),
+            connectionHost: __get(store, 'state.app.settings.connection.host'),
+            connectionType: __get(store, 'state.app.settings.connection.proxy.type'),
+            autoRefreshUsage: __get(store, 'state.app.settings.system.updates.enabled'),
+            autoRefreshTimeout: __get(store, 'state.app.settings.system.updates.timeout'),
+            torrentsProcessing: __get(store, 'state.app.settings.player.torrents.process'),
+          },
+          webvisor: true,
+          clickmap: true,
+          trackHash: true,
+          userParams: {
+            UserID: __get(store, 'state.app.account.userId'),
+          },
+          accurateTrackBounce: true,
+        }
+      })
+    }
   }
 }
