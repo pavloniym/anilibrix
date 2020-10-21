c9a822569bffa5b8839190fe65fe901a47eef864
PavelShar <PavelShar@users.noreply.github.com>
2020-10-21 15:20:55 +0000

    deploy: 6a240c9be8d2b35ba18db383c4c668ce3ac5ccae

---
+++ precache-manifest.d7c184c52bd3eeb7cdf96844b0ca751d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0d530fc5b1c8b6ee16",
+    "url": "/css/chunk-0d932a97.a7eb41e5.css"
+  },
+  {
+    "revision": "a48f6de120c6030b8878",
+    "url": "/css/chunk-37056bf3.5291c1cc.css"
+  },
+  {
+    "revision": "66b21fc3334cd9972f72",
+    "url": "/css/chunk-6c808d01.334b5a43.css"
+  },
+  {
+    "revision": "ed8baebb9f0bea235975",
+    "url": "/css/chunk-98f043d4.a1aa878f.css"
+  },
+  {
+    "revision": "58d4dc9a730cd713f749",
+    "url": "/css/chunk-f6be8f88.c3aa8424.css"
+  },
+  {
+    "revision": "f04beecf4f7975469e92",
+    "url": "/css/chunk-vendors.0be5c62d.css"
+  },
+  {
+    "revision": "0592602a6d63f4d6189f",
+    "url": "/css/index.7a854b4d.css"
+  },
+  {
+    "revision": "e788be0e4516e1384644a921388b7bb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb0d530fc5b1c8b6ee16",
+    "url": "/js/chunk-0d932a97.3c7a56f8.js"
+  },
+  {
+    "revision": "40726cf37b0e019ef6ad",
+    "url": "/js/chunk-2d0d6bbc.fca90f34.js"
+  },
+  {
+    "revision": "a48f6de120c6030b8878",
+    "url": "/js/chunk-37056bf3.1aa942c0.js"
+  },
+  {
+    "revision": "66b21fc3334cd9972f72",
+    "url": "/js/chunk-6c808d01.bed43445.js"
+  },
+  {
+    "revision": "ed8baebb9f0bea235975",
+    "url": "/js/chunk-98f043d4.e92590ea.js"
+  },
+  {
+    "revision": "b1cfe484215d10dc6057",
+    "url": "/js/chunk-c8b283ac.d9e71735.js"
+  },
+  {
+    "revision": "58d4dc9a730cd713f749",
+    "url": "/js/chunk-f6be8f88.c72282c9.js"
+  },
+  {
+    "revision": "f04beecf4f7975469e92",
+    "url": "/js/chunk-vendors.fad70ab4.js"
+  },
+  {
+    "revision": "0592602a6d63f4d6189f",
+    "url": "/js/index.272873a8.js"
+  },
+  {
+    "revision": "211c7684bba7d5c51d71",
+    "url": "/js/torrents.288e2979.js"
+  },
+  {
+    "revision": "076f730b2c24ddbcae5397d454fda6d9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "fc274f655f98870e61bb0c628524650b",
+    "url": "/torrents.html"
+  }
+]);
